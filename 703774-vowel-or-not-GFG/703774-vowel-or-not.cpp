0ae8e37728b77396810f63ed90a2f826ffe5e658
Prince Yadav <princey0703@gmail.com>
2026-09-21 23:08:20 +0530

    Vowel or Not

---
+++ 703774-vowel-or-not-GFG/703774-vowel-or-not.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char ch;
+    cin >> ch;
+
+    // code here
+    if(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U'){
+        cout<< "true";
+    
+    }
+    else{
+        cout<<"false";
+    }
+
+    return 0;
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
